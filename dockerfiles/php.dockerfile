991dac3527becc58afbfd716e5b19f9d033ad893
Raiyan <raiyan.hafiz@theright.fit>
2024-03-12 03:25:38 +0600

    setup services php, nginx, mysql, phpmyadmin

---
+++ dockerfiles/php.dockerfile
@@ -0,0 +1,8 @@
+FROM php:8.2-fpm-alpine
+
+
+WORKDIR /var/www/html
+
+COPY . .
+
+RUN docker-php-ext-install pdo pdo_mysql
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <h1>PHP Test Script</h1>
+    <p>This page was generated with PHP. The current date and time is: <?php echo date('Y-m-d H:i:s'); ?>.</p>
+</body>
+</html>
